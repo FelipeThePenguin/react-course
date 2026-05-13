b3e963ee1f407587436b5ee22d1386db3f899b78
FelipeThePenguin <chrisrockhateseveryone@gmail.com>
2026-05-13 09:33:21 +0800

    Exercise 6j

--- ecommerce-project/src/pages/OrdersPage.jsx
+++ ecommerce-project/src/pages/OrdersPage.jsx
@@ -1,5 +1,6 @@
 import { Header } from "../components/Header";
 import { Link } from 'react-router';
+import BuyAgain from '../assets/images/icons/buy-again.png';
 import "./OrdersPage.css";
 
 export function OrdersPage() {
@@ -49,7 +50,7 @@ export function OrdersPage() {
                 <button className="buy-again-button button-primary">
                   <img
                     className="buy-again-icon"
-                    src="images/icons/buy-again.png"
+                    src={BuyAgain}
                   />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
@@ -78,7 +79,7 @@ export function OrdersPage() {
                 <button className="buy-again-button button-primary">
                   <img
                     className="buy-again-icon"
-                    src="images/icons/buy-again.png"
+                    src={BuyAgain}
                   />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
@@ -127,7 +128,7 @@ export function OrdersPage() {
                 <button className="buy-again-button button-primary">
                   <img
                     className="buy-again-icon"
-                    src="images/icons/buy-again.png"
+                    src={BuyAgain}
                   />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
